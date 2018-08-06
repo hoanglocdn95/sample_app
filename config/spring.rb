17f9265a7471a5106d93a697309146cb23f5134b
hoanglocdn95 <hoanglocdn95@gmail.com>
2018-08-06 09:28:40 +0700

    chap 5 merge

--- config/spring.rb
+++ config/spring.rb
@@ -1,6 +1,7 @@
-%w[
+%w(
+>>>>>>> 4fb13d7... chap 5 done
   .ruby-version
   .rbenv-vars
   tmp/restart.txt
   tmp/caching-dev.txt
-].each { |path| Spring.watch(path) }
+).each{|path| Spring.watch(path)}
